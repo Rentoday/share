0d1648308341e2bae4acdd6be79f4d83ab4e6611
Mongdori00 <mongdori00@naver.com>
2024-07-16 20:34:24 +0900

    Notice

--- src/main/java/com/project/rentoday/domain/notice/entity/Notice.java
+++ src/main/java/com/project/rentoday/domain/notice/entity/Notice.java
@@ -6,13 +6,11 @@ import com.project.rentoday.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 @Entity
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notice extends BaseEntity {
 
     @Id
