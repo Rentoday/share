0d4a4005e3c4d448bca7f953a15db72d48fd1c16
Qocs <ahdehfl1004@gmail.com>
2024-08-09 13:15:31 +0900

    [feature/pay cancelled]

--- src/main/java/com/project/rentoday/domain/payment/controller/PayController.java
+++ src/main/java/com/project/rentoday/domain/payment/controller/PayController.java
@@ -1,69 +1,69 @@
-package com.project.rentoday.domain.payment.controller;
+    package com.project.rentoday.domain.payment.controller;
 
-import com.project.rentoday.domain.payment.dto.request.PayCallbackRequestDto;
-import com.project.rentoday.domain.payment.dto.request.PayRequestDto;
-import com.project.rentoday.domain.payment.dto.response.PayInfoResponse;
-import com.project.rentoday.domain.payment.service.PayService;
-import io.swagger.annotations.Api;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+    import com.project.rentoday.domain.payment.dto.request.PayCallbackRequestDto;
+    import com.project.rentoday.domain.payment.dto.request.PayRequestDto;
+    import com.project.rentoday.domain.payment.dto.response.PayInfoResponse;
+    import com.project.rentoday.domain.payment.service.PayService;
+    import io.swagger.annotations.Api;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.data.domain.Page;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.security.core.annotation.AuthenticationPrincipal;
+    import org.springframework.security.core.userdetails.UserDetails;
+    import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+    import java.util.List;
 
-@RestController
-@RequiredArgsConstructor
-@Api(tags = "Pay")
-@RequestMapping("/api/pays")
-public class PayController {
+    @RestController
+    @RequiredArgsConstructor
+    @Api(tags = "Pay")
+    @RequestMapping("/api/pays")
+    public class PayController {
 
-    private final PayService payService;
+        private final PayService payService;
 
-    @PostMapping("/request")
-    public ResponseEntity<PayRequestDto> requestPay(@RequestBody PayRequestDto requestDto) {
-        PayRequestDto payRequestDto = payService.requestPay(requestDto.getReservationUid());
-        return ResponseEntity.ok(payRequestDto);
-    }
+        @PostMapping("/request")
+        public ResponseEntity<PayRequestDto> requestPay(@RequestBody PayRequestDto requestDto) {
+            PayRequestDto payRequestDto = payService.requestPay(requestDto.getReservationUid());
+            return ResponseEntity.ok(payRequestDto);
+        }
 
-    @PostMapping("/callback")
-    public ResponseEntity<String> handlePayCallback(@RequestBody PayCallbackRequestDto callback) {
-        payService.payByCallback(callback);
-        return ResponseEntity.ok("결제가 성공적으로 진행되었습니다.");
-    }
+        @PostMapping("/callback")
+        public ResponseEntity<String> handlePayCallback(@RequestBody PayCallbackRequestDto callback) {
+            payService.payByCallback(callback);
+            return ResponseEntity.ok("결제가 성공적으로 진행되었습니다.");
+        }
 
-    @GetMapping("/callback")
-    public ResponseEntity<String> handlePaymentCallback(
-            @RequestParam("imp_uid") String impUid,
-            @RequestParam("merchant_uid") String merchantUid) {
-        try {
-            PayCallbackRequestDto requestDto = new PayCallbackRequestDto(impUid, merchantUid);
-            payService.payByCallback(requestDto);
-            return ResponseEntity.ok().body("Payment processed successfully");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error processing payment: " + e.getMessage());
+        @GetMapping("/callback")
+        public ResponseEntity<String> handlePaymentCallback(
+                @RequestParam("imp_uid") String impUid,
+                @RequestParam("merchant_uid") String merchantUid) {
+            try {
+                PayCallbackRequestDto requestDto = new PayCallbackRequestDto(impUid, merchantUid);
+                payService.payByCallback(requestDto);
+                return ResponseEntity.ok().body("Payment processed successfully");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("Error processing payment: " + e.getMessage());
+            }
+        }
+
+        //멤버별 결제 내역
+        @GetMapping("/member")
+        public Page<PayInfoResponse> getPaymentsByMember(
+                @AuthenticationPrincipal UserDetails principal,
+                @RequestParam(defaultValue = "0") int page,
+                @RequestParam(defaultValue = "10") int size) {
+            String email = principal.getUsername();
+            return payService.getPaymentsByMember(email, page, size);
+        }
+
+        @PostMapping("/cancel/{impUid}")
+        public ResponseEntity<String> cancelPayment(@PathVariable String impUid) {
+            payService.cancelPaymentByImpUid(impUid);
+            return ResponseEntity.ok("결제가 성공적으로 취소되었습니다.");
         }
     }
-
-    //멤버별 결제 내역
-    @GetMapping("/member")
-    public Page<PayInfoResponse> getPaymentsByMember(
-            @AuthenticationPrincipal UserDetails principal,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) {
-        String email = principal.getUsername();
-        return payService.getPaymentsByMember(email, page, size);
-    }
-
-    @PostMapping("/cancel/{impUid}")
-    public ResponseEntity<String> cancelPayment(@PathVariable String impUid) {
-        payService.cancelPaymentByImpUid(impUid);
-        return ResponseEntity.ok("결제가 성공적으로 취소되었습니다.");
-    }
-}
 
 
--- src/main/java/com/project/rentoday/domain/payment/service/PayService.java
+++ src/main/java/com/project/rentoday/domain/payment/service/PayService.java
@@ -49,6 +49,8 @@ public class PayService {
     private final ApplicationEventPublisher publisher;
     private final RedisMessagePublisher redisMessagePublisher;
 
+    private static final int MAX_RETRY_COUNT = 3;  // 최대 3번 재시도
+    private static final long RETRY_DELAY = 2000L; // 2초(2000 밀리초) 대기
 
     public PayRequestDto requestPay(String reservationUid) {
         Reservation reservation = reservationRepository.findReservationAndPayAndMember(reservationUid)
@@ -117,10 +119,11 @@ public class PayService {
             Pay pay = payRepository.findByImpUid(impUid)
                     .orElseThrow(() -> new IllegalArgumentException("해당 결제 내역이 없습니다."));
 
-            // 취소 요청
+            // 환불 요청
             CancelData cancelData = new CancelData(iamportPayment.getImpUid(), true, BigDecimal.valueOf(pay.getAmount()));
             IamportResponse<Payment> cancelResponse = iamportClient.cancelPaymentByImpUid(cancelData);
 
+            // 환불 상태 확인 및 처리
             if (cancelResponse.getResponse().getStatus().equals("cancelled")) {
                 pay.changePayByCancel(PaymentStatus.CANCELLED);
                 payRepository.save(pay);
@@ -135,8 +138,60 @@ public class PayService {
                 throw new RuntimeException("결제 취소에 실패했습니다.");
             }
         } catch (IamportResponseException | IOException e) {
-            throw new RuntimeException("결제 취소 중 오류가 발생했습니다.", e);
+            log.error("결제 취소 중 오류가 발생했습니다.", e);
+
+            // 환불 실패 시 자동 재시도 로직 실행
+            retryCancelPayment(impUid);
         }
+    }
+
+    private void retryCancelPayment(String impUid) {
+        int retryCount = 0; // 재시도 횟수
+        boolean success = false; // 기본값
+
+        while (retryCount < MAX_RETRY_COUNT && !success) {
+
+            try {
+                //일정 시간 대기 후 재시도
+                Thread.sleep(RETRY_DELAY);
+                IamportResponse<Payment> paymentIamportResponse = iamportClient.paymentByImpUid(impUid);
+                Payment iamportPayment = paymentIamportResponse.getResponse();
+
+                //환불 요청 재시도
+                CancelData cancelData = new CancelData(
+                        iamportPayment.getImpUid(),
+                        true,
+                        BigDecimal.valueOf(iamportPayment.getAmount().doubleValue()));
+                IamportResponse<Payment> cancelResponse = iamportClient.cancelPaymentByImpUid(cancelData);
+
+                if (cancelResponse.getResponse().getStatus().equals("cancelled")) {
+                    success = true;
+                    //결제 취소 성공 처리
+                    handleSuccessfulCancellation(iamportPayment);
+                }
+            } catch (IamportResponseException | IOException | InterruptedException exception) {
+                log.error("환불 재시도 중 오류가 발생했습니다.", exception);
+                retryCount++;
+            }
+        }
+        if (!success) {
+            sendErrorNotification("환불 실패", new RuntimeException("최대 재시도 횟수를 초과했습니다."));
+        }
+    }
+
+    private void handleSuccessfulCancellation(Payment iamportPayment) {
+        //환불 성공 시 DB 업데이트
+        Pay pay = payRepository.findByImpUid(iamportPayment.getImpUid())
+                .orElseThrow(() -> new IllegalArgumentException("해당 결제 내역이 없습니다."));
+        pay.changePayByCancel(PaymentStatus.CANCELLED);
+        payRepository.save(pay);
+
+        Reservation reservation = pay.getReservation();
+        reservation.setStatus(ReservationStatus.CANCEL);
+        reservationRepository.save(reservation);
+
+        //사용자에게 결제 취소 및 환불 성공 알림 전송
+        sendPaymentCancellationNotification(reservation);
     }
 
     private Pay savePaymentInfo(Reservation reservation, Payment payment) {
